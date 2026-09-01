47aadee0e8ac843b67200be3c97364a10dc8e2fe
Cledison Viana <cledison.viana.ti@gmail.com>
2026-09-01 09:07:37 -0300

    Delete src directory

--- src/routes/authRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { cadastrar, login } = require('../controllers/authController');
-
-router.post('/cadastro', cadastrar);
-router.post('/login', login);
-
-module.exports = router;
--- src/routes/relatorioRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { resumoFinanceiro } = require('../controllers/relatorioController');
-
-router.get('/financeiro', resumoFinanceiro);
-
-module.exports = router;
